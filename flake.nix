40df210c7c65d55c6fbb56c403aef117205ad360
luis <ldesiqueira@pinger.com>
2024-01-20 19:16:58 -0300

    init flake.nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
 {
-  description = "nix packages";
+  description = "A flake for auxilliary packages";
+
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { ... }: { };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem flake-utils.lib.allSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ ];
+        };
+
+        myPackage = pkgs.callPackage ./path/to/my-package { };
+      in
+
+      {
+        packages = {
+          my-package = myPackage;
+        };
+      }
+    );
 }
+
